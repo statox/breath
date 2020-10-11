6cd8d02c6aca1f978b96f2479f511955b0c2191d
Adrien Fabre <adrien.fabre@dashlane.com>
2020-10-11 21:19:02 +0200

    First try of background waves

--- app/animations/SimpleCircle.js
+++ app/animations/SimpleCircle.js
@@ -2,6 +2,10 @@ function SimpleCircle() {
     this.minR = 10;
     this.maxR = 200;
     this.draw = (percentage) => {
+        // backgroundColorChange(percentage);
+        background(50);
+        backgroundWaves(percentage);
+
         const r = map(percentage, 0, 100, this.minR, this.maxR);
         circle(width / 2, height / 2, r);
     };
--- app/animations/animationHelpers.js
+++ app/animations/animationHelpers.js
@@ -4,3 +4,27 @@ function backgroundColorChange(percentage) {
     const b = 200;
     background(r, g, b);
 }
+
+/*
+ * Shamelessly stolen from Clarke Cribb on here:
+ * https://stackoverflow.com/q/63703142/4194289
+ */
+function backgroundWaves(percentage) {
+    noFill();
+    strokeWeight(1);
+    const ranges = 100;
+
+    for (let i = 1; i < ranges; i++) {
+        let paint = map(i, 0, ranges, 1, 105);
+        stroke(paint);
+
+        beginShape();
+        for (let x = -100; x < width + 11; x += 20) {
+            const n = noise(x * 0.001, i * 0.01, frameCount * 0.01);
+            const noff = map(percentage, 100, 0, 50, height);
+            const y = map(n, 0.1, 1, noff, height);
+            vertex(x, y);
+        }
+        endShape();
+    }
+}
