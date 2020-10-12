cf4579b7f0257fbbe7ac8727f52dda8184ab0f3b
Adrien Fabre <adrien.fabre@dashlane.com>
2020-10-12 13:49:43 +0200

    Tweak PolygonAngle

--- app/animations/PolygonAngle.js
+++ app/animations/PolygonAngle.js
@@ -4,6 +4,7 @@ function PolygonAngle(x, y, maxAngle) {
     this.maxAngle = 2 * PI;
 
     this.draw = (percentage) => {
+        this.mag = (Math.min(width, height) * 0.8) / 2;
         backgroundColorChange(percentage);
         fill('rgba(100, 150, 100, 0.4)');
 
@@ -15,7 +16,7 @@ function PolygonAngle(x, y, maxAngle) {
 
         const vertexes = [];
         const v = new p5.Vector(0, -10);
-        v.setMag(100);
+        v.setMag(this.mag);
 
         for (let i = 0; i <= nbVertexes; i++) {
             vertexes.push(v.copy());
